6bdbdc77c8bac6b90b4e896fa163af7595ff25a3
Sumit Khandelwal <sumitgniit@gmail.com>
2021-07-20 12:08:44 +0530

    delete operataion added

--- 01-graphql-basics/src/resolvers/Mutation.js
+++ 01-graphql-basics/src/resolvers/Mutation.js
@@ -16,6 +16,23 @@ const Mutation = {
         db.users.push(newUser)
         return newUser;
     },
+    deleteUser(parent, args , {db}, ctx){
+        const position = db.users.findIndex(user => user.id === args.id)
+        if(position === -1){
+            throw new Error("User does not exist")
+        }
+        const [deletedUser] = db.users.splice(position, 1)
+
+        db.posts = db.posts.filter(post => {
+            const matched = post.author === args.id
+            if(matched){
+                db.comments = db.comments.filter(comment => comment.post !== post.id)
+            }
+            return !matched;
+        })
+        db.comments = db.comments.filter(comment => comment.author !== args.id)
+        return deletedUser;
+    },
     createPost(parent, args, {db}, info){
         const {title, body, published, authorId} = args.data;
         const match = db.users.some(user => user.id === authorId)
@@ -28,6 +45,17 @@ const Mutation = {
         db.posts.push(newPost)
         return newPost
     },
+    deletePost(parent, args, {db}, info){
+        const position = db.posts.findIndex(post => post.id == args.id)
+        if(position === -1){
+            throw new Error("Unable to delete post")
+        }
+        const [deletedPost] = db.posts.splice(position, 1)
+
+        db.comments = db.comments.filter(comment => comment.post !== args.id)
+
+        return deletedPost;
+    },
     createComment(parent, args, {db}, info){
         const {text, postId, authorId} = args.data;
         const userExists = db.users.some(user => user.id === authorId)
@@ -38,7 +66,17 @@ const Mutation = {
         const newComment = { id : v4(), text, post : postId, author : authorId}
         db.comments.push(newComment)
         return newComment
+    },
+    deleteComment(parent, args, {db}, info){
+        const position = db.comments.findIndex(comment => comment.id === args.id)
+        if(position === -1)
+        {
+            throw new Error("Unable to delete commen")
+        }
+        const [deletedComment]= db.comments.splice(position, 1)
+        return deletedComment;
     }
+    
 }
 
 export default Mutation
